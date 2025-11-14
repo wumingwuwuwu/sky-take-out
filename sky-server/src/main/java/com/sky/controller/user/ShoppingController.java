1b58f0b5add022917e78e5fd2c954abd5c45fbee
wuzeyu <1743824272@qq.com>
2025-11-14 21:48:59 +0800

    用户端-购物车-添加购物车

--- sky-server/src/main/java/com/sky/controller/user/ShoppingController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user/shop")
 @Api(tags = "店铺相关接口")
 @Slf4j
-public class ShopController {
+public class ShoppingController {
 
     public static final String KEY = "SHOP_STATUS";
 
